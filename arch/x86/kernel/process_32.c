3195724c3a33eca4606f6f20b5c4a5bd6443a13e
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-01-11 10:45:43 -0500

    idle-notifier-x86_32-fix
    idle notifier x86_32 fix
    
    - Comment cleanup
    - Fix apm.c for 32-bit. Need to include asm/idle.h and need __exit_idle symbol.
      Therefore, x86 64 __exit_idle must become non-static too.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- arch/x86/kernel/process_32.c
+++ arch/x86/kernel/process_32.c
@@ -76,7 +76,7 @@ void enter_idle(void)
 	notify_idle(IDLE_START);
 }
 
-static void __exit_idle(void)
+void __exit_idle(void)
 {
 	if (x86_test_and_clear_bit_percpu(0, is_idle) == 0)
 		return;
@@ -146,9 +146,11 @@ void cpu_idle(void)
 			stop_critical_timings();
 			pm_idle();
 			start_critical_timings();
-			/* In many cases the interrupt that ended idle
-			   has already called exit_idle. But some idle
-			   loops can be woken up without interrupt. */
+			/*
+			 * In many cases the interrupt that ended idle
+			 * has already called exit_idle. But some idle
+			 * loops can be woken up without interrupt.
+			 */
 			__exit_idle();
 			trace_power_end(smp_processor_id());
 		}
