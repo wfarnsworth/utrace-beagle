89e64fbe0d53b6a31924d2cfd0dbcda09194f6dd
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-01-11 10:45:31 -0500

    lttng-pm-idle-instrumentation
    LTTng pm_idle instrumentation, arch-specific
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- arch/x86/kernel/process_64.c
+++ arch/x86/kernel/process_64.c
@@ -37,6 +37,7 @@
 #include <linux/uaccess.h>
 #include <linux/io.h>
 #include <linux/ftrace.h>
+#include <trace/pm.h>
 
 #include <asm/pgtable.h>
 #include <asm/system.h>
@@ -52,6 +53,9 @@
 #include <asm/debugreg.h>
 
 #include <trace/events/power.h>
+
+DEFINE_TRACE(pm_idle_exit);
+DEFINE_TRACE(pm_idle_entry);
 
 asmlinkage extern void ret_from_fork(void);
 
@@ -75,6 +79,11 @@ EXPORT_SYMBOL_GPL(idle_notifier_unregister);
 void enter_idle(void)
 {
 	percpu_write(is_idle, 1);
+	/*
+	 * Trace last event before calling notifiers. Notifiers flush
+	 * data from buffers before going to idle.
+	 */
+	trace_pm_idle_entry();
 	atomic_notifier_call_chain(&idle_notifier, IDLE_START, NULL);
 }
 
@@ -83,6 +92,7 @@ static void __exit_idle(void)
 	if (x86_test_and_clear_bit_percpu(0, is_idle) == 0)
 		return;
 	atomic_notifier_call_chain(&idle_notifier, IDLE_END, NULL);
+	trace_pm_idle_exit();
 }
 
 /* Called from interrupts to signify idle end */
