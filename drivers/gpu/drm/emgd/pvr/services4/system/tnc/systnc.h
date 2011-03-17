2e881adabe2279763976d44ea478c62e56fd5995
Tom Zanussi <tom.zanussi@intel.com>
2011-03-17 13:03:34 -0500

    crownbay: update a handful of EMGD licenses
    The version of the EMGD driver we use had a few included license
    mistakes.  Correct these as per upstream.
    
    Signed-off-by: Tom Zanussi <tom.zanussi@intel.com>

--- drivers/gpu/drm/emgd/pvr/services4/system/tnc/systnc.h
+++ drivers/gpu/drm/emgd/pvr/services4/system/tnc/systnc.h
@@ -1,40 +1,32 @@
 /**********************************************************************
- *
- * Copyright(c) 2008 Imagination Technologies Ltd. All rights reserved.
- * 
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- * 
- * This program is distributed in the hope it will be useful but, except 
- * as otherwise stated in writing, without any warranty; without even the 
- * implied warranty of merchantability or fitness for a particular purpose. 
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
- * 
- * The full GNU General Public License is included in this distribution in
- * the file called "COPYING".
- *
- * Contact Information:
- * Imagination Technologies Ltd. <gpl-support@imgtec.com>
- * Home Park Estate, Kings Langley, Herts, WD4 8LZ, UK 
- *
+ Copyright (c) Imagination Technologies Ltd.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
  ******************************************************************************/
-/* INTEL CONFIDENTIAL
- * Copyright 2010 Intel Corporation All Rights Reserved.
- */
 
 #ifndef _SYSTNC_H
 #define _SYSTNC_H
 
-#define SYS_SGX_DEV_DEVICE_ID_TNC	0x4108	
+#define SYS_SGX_DEV_DEVICE_ID_TNC	0x4108
 #define VS_PRODUCT_NAME_TNC     	"SGX Atom E6xx"
 #define SGX_REGS_OFFSET_TNC     	0x80000
 #define MSVDX_REGS_OFFSET_TNC   	0x90000
 #define SYS_SGX_CLOCK_SPEED_TNC                     (400000000)
 #define SYS_SGX_ACTIVE_POWER_LATENCY_MS_TNC         (50)
 #endif
-
