f19efa9097c8c9ec4ffb9f4a2fefbaca45d6933e
David Anders <x0132446@ti.com>
2011-03-30 16:48:01 -0500

    omap4: pandaboard: fix dvi support
    fetch patch from
    http://www.elinux.org/images/9/97/0001-omap4-pandaboard-fix-dvi-support.patch
    
    this patch fixes some clock issues, provides a base 720p dvi panel
    and adds the panda_dvi_defconfig for testing purposes.
    
    Integrated-by: Jingdong Lu <jingdong.lu@windriver.com>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -528,7 +528,7 @@ static void omap4_panda_disable_dvi(struct omap_dss_device *dssdev)
 
 /* Using generic display panel */
 static struct panel_generic_dpi_data omap4_dvi_panel = {
-        .name                   = "generic",
+        .name                   = "dvi_720p",
         .platform_enable        = omap4_panda_enable_dvi,
         .platform_disable       = omap4_panda_disable_dvi,
 };
@@ -556,6 +556,7 @@ int __init omap4_panda_dvi_init(void)
         return r;
 }
 
+#ifdef CONFIG_OMAP4_DSS_HDMI
 static void omap4_panda_hdmi_mux_init(void)
 {
 	/* PAD0_HDMI_HPD_PAD1_HDMI_CEC */
@@ -615,6 +616,12 @@ static struct omap_dss_device *omap4_panda_dss_devices[] = {
 	&omap4_panda_hdmi_device,
 };
 
+#else
+static struct omap_dss_device *omap4_panda_dss_devices[] = {
+	&omap4_panda_dvi_device,
+};
+#endif
+
 static struct omap_dss_board_info omap4_panda_dss_data = {
 	.num_devices	= ARRAY_SIZE(omap4_panda_dss_devices),
 	.devices	= omap4_panda_dss_devices,
@@ -629,7 +636,9 @@ void omap4_panda_display_init(void)
 	if (r)
 		pr_err("error initializing panda DVI\n");
 
+#ifdef CONFIG_OMAP4_DSS_HDMI
 	omap4_panda_hdmi_mux_init();
+#endif
 	omap_display_init(&omap4_panda_dss_data);
 }
 
